26d967051f7222ced4046dbbd120644e809bd6f2
Taylor Radtke <taylorradtke@gmail.com>
2021-08-16 09:57:38 -0500

    images api

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -19,9 +19,24 @@ const weather = async (req, res) => {
           county: administrativeLevels.level1short,
           latitude,
           longitude,
-        })),
+          query: `${city}, ${administrativeLevels.level1long}, ${administrativeLevels.level2long}, cityscape, scenery, city`,
+        }))
+        .then(async ({ query, ...payload }) =>
+          axios("https://api.pexels.com/v1/search", {
+            headers: {
+              Authorization: process.env.PEXELS_API_KEY,
+            },
+            params: {
+              query,
+              per_page: 1,
+            },
+          }).then((item) => ({
+            ...payload,
+            image: item.data.photos[0].src.original,
+          }))
+        ),
       axios(
-        `https://api.darksky.net/forecast/b51dd7257f9524793206bc4d5edca00e/${latitude},${longitude}`
+        `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${latitude},${longitude}`
       ),
     ]);
 
